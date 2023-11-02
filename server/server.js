0e64ed374dc7fcc386eeff9df7b112ddd67c66b2
matshp0 <143942706+matshp0@users.noreply.github.com>
2023-11-02 15:05:03 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -97,5 +97,5 @@ const server = http.createServer((req, res) =>{
 });
 
 server.listen(PORT, () => {
-    console.log('Server is running');
+    console.log(`Server is running on port: ${PORT}`);
 });
